946e48663337dd4ec2f593c1c8f6bb6d46f787ad
vmcguire <victor.mcguire@gmail.com>
2021-03-08 01:50:36 -0800

    updating generate webpage capability

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const generatePage = require('./src/page-template');
+const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
 
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
+fs.writeFile("index.html", generatePage(name, github), (err) => {
   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
+  console.log("Portfolio complete! Check out index.html to see the output!");
 });
