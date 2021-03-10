2eb87b8e6da693335bcc66cc33bdbc9783e3f1b8
vmcguire <victor.mcguire@gmail.com>
2021-03-09 20:50:53 -0800

    finished site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
 const generatePage = require("./src/page-template");
 const inquirer = require("inquirer");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
 //for testing purposes
 // const mockData = {
@@ -140,14 +140,20 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    // const pageHTML = generatePage(mockData);
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-      console.log(
-        "Page created! Check out index.html in this directory to see it!"
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
 // const fs = require("fs");
